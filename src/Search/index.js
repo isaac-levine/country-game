7bb1a8ef7dc94d0324e5d3e698df7bf1b0109374
bengardner1 <bengardner433@gmail.com>
2023-11-19 20:26:07 -0500

    Search pretty good

--- src/Search/index.js
+++ src/Search/index.js
@@ -4,16 +4,17 @@ import SearchFilters from "./SearchFilters";
 import "./index.css";
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import SearchResults from "./SearchResults"; 
-import CountryList from "./SearchResults/searchTest";
+
 
 
 function Search() {
     const [searchTerm, setSearchTerm] = useState('');
+    const [displayCount, setDisplayCount] = useState(20);
       
     const handleSearch = (term) => {
         console.log("handlesearch");
         setSearchTerm(term);
+        setDisplayCount(5); 
     };
 
     const [results, setResults] = useState([]);
@@ -37,9 +38,16 @@ function Search() {
         fetchAndDisplayResults();
     }, []);
 
-    const sortedTop20 = results
-          .sort((a, b) => a.name.common.localeCompare(b.name.common))
-          .slice(0, 20);
+    const sortedResults = results
+        .sort((a, b) => a.name.common.localeCompare(b.name.common))
+        .filter((country) =>
+            searchTerm === '' || country.name.common.toLowerCase().includes(searchTerm.toLowerCase()));
+    const visibleResults = sortedResults.slice(0, displayCount);
+    const nextResults = sortedResults.slice(displayCount, displayCount + 20);
+    const handleLoadMore = () => {
+        const visibleResults = sortedResults.slice(displayCount, displayCount + 20);
+        setDisplayCount(displayCount + 20);
+      };
 
     return(
         <div>
@@ -51,14 +59,20 @@ function Search() {
                     <div className="col-md-6">
                     <SearchBar onSearch={handleSearch} />
                     <ul className="list-group mt-5">
-                    {sortedTop20.map(country => ( //Only showing top 20 results by default
+                    {visibleResults.map(country => ( //Only showing top 20 results by default
                     <li key={country.cca2} className="list-group-item">
-                        <Link to={`/Search/${country.cca2}`}><strong>{country.name.common} {country.flag}</strong></Link>
+                        <Link className="d-inline me-2 cg-link" to={`/Search/${country.cca2}`}><strong>{country.name.common} </strong></Link>
+                        {country.flag} <br/>
                         <p>Capital: {country.capital}<br/>
                         Region: {country.region} </p>
                     </li>
                     ))}
                 </ul>
+                {nextResults.length > 0 && (
+                    <button className="btn btn-primary mt-3" onClick={handleLoadMore}>
+                    Load More
+                    </button>
+                )}
                     </div>
                     <div className="col-md-3">
                         <SearchFilters/>
